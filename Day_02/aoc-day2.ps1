b672172150875aba455532cdafc3b03ba165ed5e
deanbunn <deanbunn@gmail.com>
2023-12-01 22:41:22 -0800

    Day 2

---
+++ Day_02/aoc-day2.ps1
@@ -0,0 +1,110 @@
+<# 
+    Script: aoc-day2.ps1
+    Author: deanbunn
+#>
+
+#Var for Games Possible Total
+[int]$nGamesPossTotal = 0;
+
+#Var for Game Power Total
+[int]$nGamesPossPowerTotal = 0;
+
+foreach($flCubeGame in (Get-Content -Path ./input.txt))
+{
+
+    #Var for Game Number
+    [string]$nGameNbr = $flCubeGame.ToString().Split(":")[0].Replace("Game ","");
+
+    #Var for Game Set Raw
+    [string]$strGameSetRaw = $flCubeGame.ToString().Split(":")[1];
+
+    #Var for Game Possible
+    $bGamePossible = $true;
+
+    #Var for Required Red Cubes
+    [int]$nRequiredCubesRed = 0;
+
+    #Var for Required Green Cubes
+    [int]$nRequiredCubesGreen = 0;
+
+    #Var for Required Blue Cubes 
+    [int]$nRequiredCubesBlue = 0;
+
+    foreach($gameSet in $strGameSetRaw.ToString().Split(";"))
+    {
+        #Var for Number Red Cubes
+        [int]$nCubesRed = 0;
+
+        #Var for Number Green Cubes
+        [int]$nCubesGreen = 0;
+
+        #Var for Number Blue Cubes
+        [int]$nCubesBlue = 0;
+
+        foreach($gSet in $gameSet.ToString().Split(","))
+        {
+
+            if($gSet.Contains("red"))
+            {
+                $nCubesRed += [int]($gSet.Replace(" red","").Trim())
+            }
+
+            if($gSet.Contains("green"))
+            {
+                $nCubesGreen += [int]($gSet.Replace(" green","").Trim())
+            }
+
+            if($gSet.Contains("blue"))
+            {
+                $nCubesBlue += [int]($gSet.Replace(" blue","").Trim())
+            }
+             
+        }
+
+        #Check Game Set for Possible Game
+        if($nCubesRed -gt 12 -or $nCubesGreen -gt 13 -or $nCubesBlue -gt 14)
+        {
+            $bGamePossible = $false;
+        }
+
+        #Check Required Red Cubes Values
+        if($nRequiredCubesRed -lt $nCubesRed)
+        {
+            $nRequiredCubesRed = $nCubesRed;
+        }
+
+        #Check Required Green Cubes Values
+        if($nRequiredCubesGreen -lt $nCubesGreen)
+        {
+            $nRequiredCubesGreen = $nCubesGreen;
+        }
+
+        #Check Required Blue Cubes Values
+        if($nRequiredCubesBlue -lt $nCubesBlue)
+        {
+            $nRequiredCubesBlue = $nCubesBlue;
+        }
+        
+    }
+
+    if($bGamePossible -eq $true)
+    {
+        $nGamesPossTotal += [int]$nGameNbr;
+    }
+    
+    $nGamesPossPowerTotal += ($nRequiredCubesRed * $nRequiredCubesGreen * $nRequiredCubesBlue)
+
+}
+
+$nGamesPossTotal.ToString();
+
+$nGamesPossPowerTotal.ToString();
+
+<#
+Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games?
+---------------
+
+The power of a set of cubes is equal to the numbers of red, green, and blue cubes multiplied together. The power of the minimum set of cubes in game 1 is 48. In games 2-5 it was 12, 1560, 630, and 36, respectively. Adding up these five powers produces the sum 2286.
+
+For each game, find the minimum set of cubes that must have been present. What is the sum of the power of these sets?
+#>
